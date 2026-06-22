cd2d3e6b3dc896c794f76b4656e47c047055a2f0
phuc1nguyen <phuc1nguyen@proton.me>
2026-06-22 13:40:01 +0700

    variables exercises

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -2,7 +2,6 @@ fn main() {
     let number = "T-H-R-E-E"; // Don't change this line
     println!("Spell a number: {number}");
 
-    // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    let number: u32 = 3; // Use variable shadowing
     println!("Number plus two is: {}", number + 2);
 }
